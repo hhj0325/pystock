015201bbf718aadef32e608b773752019e92bc15
huhaojian <wwang0325@yeah.net>
2017-04-09 20:23:07 +0800

    before numpy test

--- com/hhj/pystock/demo/__init__.py
+++ com/hhj/pystock/demo/__init__.py
@@ -1,1 +1,19 @@
+def square(x):
+    return x * x
+
+
+def f(a, b=1, c="hehe"):
+    return a + b
+
 print("Hello world")
+print(square(9))
+print(f(2))
+
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
+
+b = [1, 2, 3, 4]
+for i in b:
+    print(i)
--- com/hhj/pystock/master/__init__.py
+++ com/hhj/pystock/master/__init__.py
@@ -1,1 +1,10 @@
-print("aaaaa")
+class boy:
+    gender = 'male'
+    interest = 'girl'
+
+    def say(self):
+        return 'i am a boy'
+
+
+hhj = boy()
+print(hhj.say())
