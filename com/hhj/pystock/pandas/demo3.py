e9c5b96b8e5c56a9e51e7714deb0322fc9fe9317
huhaojian <huhaojian@jd.com>
2017-08-28 14:38:56 +0800

    各种demo

--- com/hhj/pystock/pandas/demo3.py
+++ com/hhj/pystock/pandas/demo3.py
@@ -3,15 +3,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-# ts = ts.cumsum()
-# ts.plot()
+ts = ts.cumsum()
+ts.plot()
 
 df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,
                   columns=['A', 'B', 'C', 'D'])
-# df = df.cumsum()
-# plt.figure();
-# df.plot();
-# plt.legend(loc='best')
+df = df.cumsum()
+plt.figure()
+df.plot()
+plt.legend(loc='best')
+plt.show()
 
-df.to_excel('foo.xlsx', sheet_name='Sheet1')
+# df.to_excel('foo.xlsx', sheet_name='Sheet1')
 
